04ab412d84c1b257543a4ca1ebbf64719ece20a9
naseeb-shah-01 <165763875+naseeb-shah-01@users.noreply.github.com>
2025-02-03 11:15:28 +0530

    change api url

--- src/utilis/urls.ts
+++ src/utilis/urls.ts
@@ -1,5 +1,5 @@
 export class APIUrls {
-	static readonly baseUrl: string = "http://localhost:5000";
+	static readonly baseUrl: string = "https://server-port-six.vercel.app/";
   
 	// Auth
 	static readonly authUrl: string = `${APIUrls.baseUrl}/auth`;
